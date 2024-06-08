8ccbf2d60117c1d7a8fc3ac6d666f1da281d7589
gusdn010158 <gusdn010158@gmail.com>
2024-06-08 23:24:52 +0900

    about page

--- src/Main.js
+++ src/Main.js
@@ -31,7 +31,7 @@ function Main(props) {
           </Small>
           <Soge>
             <Bigfont>Front-End</Bigfont>
-            <div> Developer</div>
+            <Bigfont> Developer</Bigfont>
           </Soge>
         </Top>
         <Pro>
@@ -44,11 +44,46 @@ function Main(props) {
           </Prorg>
         </Pro>
       </Maintop>
-      <ShowMore>쇼모얼</ShowMore>
+      <ShowMore>
+        <Showcom>
+          <h2>Introduction</h2>
+          <Showdiv>
+            안녕하세요, 저는 UI/UX을 최적화하고, 효율적이고 아름다운 웹
+            애플리케이션을 구축하는 프론트엔드 개발 지원자입니다.
+            <br /> 다양한 기술 스택을 활용하여 복잡한 문제를 창의적으로 해결하는
+            데 많은 노력을 합니다.
+            <br /> 또한, 최신 웹 개발 트렌드와 도구를 빠르게 습득하고 적용하는
+            능력이 강점입니다.
+            <br /> 저는 협업과 소통을 중요하게 생각하며, 다양한 팀 프로젝트를
+            통해 효과적으로 협력한 경험이 있습니다.
+            <br /> 경험을 통해 프로젝트의 목표를 명확히 이해하고, 최고의
+            결과물을 도출하기 위해 노력해왔습니다.
+            <br />
+            혁신적인 프로젝트와 함께 성장하며, 사용자에게 최고의 경험을 제공하는
+            데 기여하고 싶습니다.
+          </Showdiv>
+        </Showcom>
+      </ShowMore>
       <Intro>
         <Intext>Do you want to see more?</Intext>
       </Intro>
-      <Skill>스킬</Skill>
+      <Skill>
+        <Skilld>
+          <Skillh2>MY SKILL</Skillh2>
+          <div>NEXT.JS</div>
+          <div>HTML5</div>
+          <div>CSS3</div>
+          <div>TYPESCRPIT</div>
+          <div>JAVASCRPIT</div>
+          <div>REACT</div>
+          <div>REDUX</div>
+          <div>RECOIL</div>
+          <div>REACT HOOK FORM</div>
+          <div>FIGMA</div>
+          <div>GITHUB</div>
+          <div>STYLED COMPONENTS</div>
+        </Skilld>
+      </Skill>
     </div>
   );
 }
@@ -61,6 +96,15 @@ export default Main;
 //   overflow: hidden;
 //   overflow-y: auto;
 // `;
+const Skillh2 = styled.h2`
+  font-size: 40px;
+  margin: 0;
+`;
+const Showdiv = styled.div`
+  width: 1300px;
+
+  font-size: 28px;
+`;
 const Intext = styled.div`
   width: 700px;
   font-size: 150px;
@@ -93,6 +137,7 @@ const Top = styled.div`
 const Prorg = styled.div`
   color: white;
   font-size: 60px;
+  font-weight: 700;
   height: 400px;
   display: flex;
   justify-content: center;
@@ -104,6 +149,7 @@ const Pro = styled.div`
 `;
 const Prospan = styled.span`
   font-size: 130px;
+  font-weight: 900;
 `;
 const Header = styled.div`
   color: white;
@@ -118,12 +164,15 @@ const Header = styled.div`
 `;
 
 const Logo = styled(Header)`
-  margin-right: 300px;
+  margin-right: 250px;
   display: flex;
   align-items: center;
   justify-content: center;
 `;
-const Soge = styled(Header)``;
+const Soge = styled(Header)`
+  margin-left: 80px;
+  margin-right: 0;
+`;
 const Abouttop = styled(Header)`
   width: 230px;
 `;
@@ -136,7 +185,23 @@ const Component = styled.div`
   height: 100vh;
   width: 99vw;
 `;
-const Skill = styled(Component)``;
+const Skill = styled(Component)`
+  background-color: black;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+const Skilld = styled.div`
+  width: 300px;
+  height: 400px;
+
+  div {
+    margin: 10px;
+    font-size: 20px;
+  }
+`;
 
 const Intro = styled(Component)`
   background-position: center;
@@ -150,4 +215,22 @@ const Maintop = styled(Component)`
   flex-direction: column;
   justify-content: space-between;
 `;
-const ShowMore = styled(Component)``;
+const ShowMore = styled(Component)`
+  height: 50vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Showcom = styled.div`
+  width: 1800px;
+  height: 300px;
+  display: flex;
+  justify-content: center;
+  h2 {
+    font-size: 40px;
+    height: 150px;
+    margin: 0;
+    margin-right: 80px;
+  }
+`;
