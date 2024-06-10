77c901cb2e93d4ebe7cb72295f618a79c24f3b27
gusdn010158 <gusdn010158@gmail.com>
2024-06-10 23:21:07 +0900

    alone page

--- src/Collabo.js
+++ src/Collabo.js
@@ -1,13 +1,19 @@
 import React from "react";
 import Top from "./Top";
-
+import styled from "styled-components";
 function Collabo(props) {
   return (
     <>
       <Top />
-      <div>협업 프로젝트</div>
+      <TP>SWS</TP>
+      <TP>MOMUKG</TP>
+      <TP>헬린이 쇼핑몰</TP>
     </>
   );
 }
 
 export default Collabo;
+const TP = styled.div`
+  height: 100vh;
+  width: 99vw;
+`;
